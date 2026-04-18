f4898136e94fbbcd90905f11a2aec3211a1eccfe
Bondarenko Roman <bondarenkoromanupwork@gmail.com>
2026-04-18 21:12:26 +0300

    gates

--- Assets/_Game/Scripts/SplineMover/PingPongSplineMover.cs
+++ Assets/_Game/Scripts/SplineMover/PingPongSplineMover.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Dreamteck.Splines;
+
 public class PingPongSplineMover : SplineMover
 {
     protected override void Init()
@@ -10,5 +11,10 @@ public class PingPongSplineMover : SplineMover
         _follower.follow = true;
         _follower.wrapMode = SplineFollower.Wrap.PingPong;
         _follower.followSpeed = Mathf.Abs(_config.MoveSpeed);
+
+        // Только позиция по сплайну: без поворота по касательной и без сдвига по Y.
+        // _follower.applyDirectionRotation = false;
+        // _follower.motion.applyRotation = false;
+        // _follower.motion.applyPositionY = false;
     }
 }
