74c3ac6e96287d23db730ab21727e2c7c720e3f6
Bondarenko Roman <bondarenkoromanupwork@gmail.com>
2026-04-18 16:58:08 +0300

    barier config

--- Assets/_Game/Scripts/Barrier/Barrier.cs
+++ Assets/_Game/Scripts/Barrier/Barrier.cs
@@ -3,7 +3,15 @@ using UnityEngine.Events;
 
 public class Barrier : MonoBehaviour
 {
+    [SerializeField] private BarrierConfig BarrierConfig;
     [SerializeField] private UnityEvent OnTriggerMob;
+
+
+    public void Awake()
+    {
+        
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.TryGetComponent<MobEntity>(out var mob))
---
+++ Assets/_Game/Scripts/Barrier/BarrierConfig.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[CreateAssetMenu(menuName = "MobControl/BarrierConfig", fileName = "BarrierConfig")]
+public class BarrierConfig : ScriptableObject
+{
+    public int Hp = 50;
+}
